332ba0dda38b4be71ed272568a140c96ff0b0a2f
ahzahid <hannanzahid27905@gmail.com>
2026-08-22 10:15:48 +0500

    Image Gallery Project

---
+++ Day-13-Image-Gallery-Project/src/components/Buttons.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Buttons = (props) => {
+    return (
+        <div className='flex justify-center gap-6 items-center p-4'>
+            <button disabled={props.index === 1}
+                style={{
+                    cursor: props.index == 1 ? 'not-allowed' : 'pointer',
+                    opacity: props.index == 1 ? 0.5 : 1,
+                }}
+                onClick={() => {
+                    if (props.index > 1) {
+                        props.setIndex(props.index - 1)
+                    }
+                    props.setUserData([])
+                }}
+                className='bg-amber-400 text-sm cursor-pointer active:scale-90 text-black rounded px-4 py-2 font-semibold'>Prev</button>
+
+            <h2 className='text-gray-400'>page {props.index}</h2>
+            <button onClick={() => {
+                props.setIndex(props.index + 1)
+                props.setUserData([])
+            }}
+                className='bg-amber-400 text-sm cursor-pointer active:scale-90 text-black rounded px-4 py-2 font-semibold'>Next</button>
+        </div>
+    )
+}
+
+export default Buttons
